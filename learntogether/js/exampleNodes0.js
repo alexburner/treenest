11f254d140b7e2f5bb2d20f9f54942200d21478e
Alex Burner <alex.burner@gmail.com>
2015-03-24 21:22:23 -0700

    added extensive fractal nesting test data

--- learntogether/js/exampleNodes0.js
+++ learntogether/js/exampleNodes0.js
@@ -61,9 +61,123 @@ var exampleNodes = [
                                     },
                                     {
                                         "title": "Troposphere",
+
                                     },
                                     {
                                         "title": "Hydrosphere"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "title": "Troposphere",
+                        "children": [
+                            {
+                                "title": "Space Shuttle Endeavour",
+                                "subtree": [
+                                    {
+                                        "title": "Planetary boundary layer"
+                                    },
+                                    {
+                                        "title": "Troposphere",
+                                        "children": [
+                                            {
+                                                "title": "Troposphere",
+                                                "subtree": [
+                                                    {
+                                                        "title": "ocean",
+                                                        "children": [
+                                                            {
+                                                                "title": "Planetary boundary layer"
+                                                            },
+                                                            {
+                                                                "title": "Troposphere",
+                                                                "children": [
+                                                                    {
+                                                                        "title": "Space Shuttle Endeavour",
+                                                                        "children": [
+                                                                            {
+                                                                                "title": "Planetary boundary layer"
+                                                                            }
+                                                                        ]
+                                                                    },
+                                                                    {
+                                                                        "title": "Friction"
+                                                                    }
+                                                                ]
+                                                            },
+                                                            {
+                                                                "title": "Hydrosphere",
+                                                                "children": [
+                                                                    {
+                                                                        "title": "Troposphere",
+                                                                        "subtree": [
+                                                                            {
+                                                                                "title": "ocean",
+                                                                                "children": [
+                                                                                    {
+                                                                                        "title": "Planetary boundary layer"
+                                                                                    },
+                                                                                    {
+                                                                                        "title": "Troposphere",
+                                                                                        "children": [
+                                                                                            {
+                                                                                                "title": "Space Shuttle Endeavour",
+                                                                                                "children": [
+                                                                                                    {
+                                                                                                        "title": "Planetary boundary layer"
+                                                                                                    }
+                                                                                                ]
+                                                                                            },
+                                                                                            {
+                                                                                                "title": "Friction"
+                                                                                            }
+                                                                                        ]
+                                                                                    },
+                                                                                    {
+                                                                                        "title": "Hydrosphere"
+                                                                                    }
+                                                                                ]
+                                                                            }
+                                                                        ],
+                                                                        "children": [
+                                                                            {
+                                                                                "title": "Space Shuttle Endeavour"
+                                                                            },
+                                                                            {
+                                                                                "title": "Friction"
+                                                                            }
+                                                                        ]
+                                                                    }
+                                                                ]
+                                                            }
+                                                        ]
+                                                    }
+                                                ],
+                                                "children": [
+                                                    {
+                                                        "title": "Space Shuttle Endeavour"
+                                                    },
+                                                    {
+                                                        "title": "Friction"
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "title": "Hydrosphere"
+                                    }
+                                ],
+                                "children": [
+                                    {
+                                        "title": "Troposphere",
+                                        "children": [
+                                            {
+                                                "title": "Troposphere"
+                                            }
+                                        ]
                                     }
                                 ]
                             }
